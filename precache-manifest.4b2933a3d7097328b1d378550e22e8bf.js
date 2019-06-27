ea8027f07e9b3993e124fc3ca7a0c93477a0df94
SaiAshish9 <saiashish3760@gmail.com>
2019-06-28 00:35:37 +0530

    Updates

---
+++ precache-manifest.4b2933a3d7097328b1d378550e22e8bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e98c702528f103a415d203c649adb5",
+    "url": "/ColorBoxesApp/index.html"
+  },
+  {
+    "revision": "6be537cc8fe4f9ae2235",
+    "url": "/ColorBoxesApp/static/css/main.b49faef6.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/ColorBoxesApp/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "6be537cc8fe4f9ae2235",
+    "url": "/ColorBoxesApp/static/js/main.1f7600c4.chunk.js"
+  },
+  {
+    "revision": "6991d7a39df65758b31d",
+    "url": "/ColorBoxesApp/static/js/runtime~main.313bd89f.js"
+  }
+]);
